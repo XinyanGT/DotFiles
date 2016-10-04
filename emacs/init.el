707638a869f7c5aaee096f2207bac0abc989a7a3
Xinyan Yan <Xinyan Yan>
2016-10-04 10:36:50 -0400

    show path name

--- emacs/init.el
+++ emacs/init.el
@@ -8,26 +8,26 @@
 ;; Package archives
 (require 'package)
 (add-to-list 'package-archives
-  '("marmalade" . "http://marmalade-repo.org/packages/"))
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+      ;; For important compatibility libraries like cl-lib
+      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 
-; fetch the list of packages available
+					; fetch the list of packages available
 (unless package-archive-contents
-  (package-refresh-contents))
+	(package-refresh-contents))
 
-; install the missing packages
+					; install the missing packages
 (dolist (package (list 'auto-complete ))
-  (unless (package-installed-p package)
-    (package-install package)))
+	(unless (package-installed-p package)
+		(package-install package)))
 
 (when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+      (exec-path-from-shell-initialize))
 
 ;; auto update buffers
 (global-auto-revert-mode t)
@@ -36,11 +36,12 @@
 ;; ;; Git
 ;; (autoload 'magit-status "magit" nil t)
 
-;; ;; Auto complete
-;; (global-auto-complete-mode t)
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; (add-to-list 'ac-modes 'rst-mode)
+;; Auto complete
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+(ac-config-default)
+(add-to-list 'ac-modes 'rst-mode)
 
 ;; Print
 ;; Which does NOT work!!
@@ -57,31 +58,31 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
- ;; Auto indention
+;; Auto indention
 (add-hook 'c-mode-common-hook '(lambda ()
-  (local-set-key(kbd "RET") 'newline-and-indent)))
+				 (local-set-key(kbd "RET") 'newline-and-indent)))
 
 ;; Backup files
 (setq vc-make-backup-files t)
 (setq version-control t     ;; Use version numbers for backups.
-   kept-new-versions 6      ;; Number of newest versions to keep.
-   kept-old-versions 2      ;; Number of oldest versions to keep.
-   delete-old-versions t    ;; Don't ask to delete excess backup versions.
-   backup-by-copying t)     ;; Copy all files, don't rename them.
+      kept-new-versions 6      ;; Number of newest versions to keep.
+      kept-old-versions 2      ;; Number of oldest versions to keep.
+      delete-old-versions t    ;; Don't ask to delete excess backup versions.
+      backup-by-copying t)     ;; Copy all files, don't rename them.
 (setq backup-directory-alist `((".*" . ,"~/.emacs.d/backup/per-save")))
 (setq auto-save-file-name-transforms `((".*" ,"~/.emacs.d/backup/per-save t")))
 (defun force-backup-of-buffer ()
-    ;; Make a special "per session" backup at the first save of each
-    ;; emacs session.
-    (when (not buffer-backed-up)
-      ;; Override the default parameters for per-session backups.
-      (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
-            (kept-new-versions 3))
-        (backup-buffer)))
-    ;; Make a "per save" backup on each save.  The first save results in
-    ;; both a per-session and a per-save backup, to keep the numbering
-    ;; of per-save backups consistent.
-   (setq buffer-backed-up nil))
+  ;; Make a special "per session" backup at the first save of each
+  ;; emacs session.
+  (when (not buffer-backed-up)
+	;; Override the default parameters for per-session backups.
+	(let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
+	      (kept-new-versions 3))
+	  (backup-buffer)))
+  ;; Make a "per save" backup on each save.  The first save results in
+  ;; both a per-session and a per-save backup, to keep the numbering
+  ;; of per-save backups consistent.
+  (setq buffer-backed-up nil))
 
 (add-hook 'before-save-hook  'force-backup-of-buffer)
 (setq auto-save-file-name-transforms `((".*" ,"~/.emacs.d/backup/per-session t")))
@@ -120,7 +121,7 @@
   (interactive "p")
   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
 	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-	                  (t (self-insert-command (or arg 1)))))
+	(t (self-insert-command (or arg 1)))))
 ;; Compile
 (global-set-key "\C-xc" 'compile)
 
@@ -135,10 +136,24 @@
 (global-set-key (kbd "C-c k")    'windmove-up)
 (global-set-key (kbd "C-c j")  'windmove-down)
 
+
 ;; Change between window layouts
 (winner-mode 1)
 (global-set-key (kbd "C-c [")  'winner-undo)
 (global-set-key (kbd "C-c ]") 'winner-redo)
+
+;; Display file path
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+	    '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
+
+(defun show-file-name ()
+  (interactive)
+  (message (buffer-file-name)))
+(global-set-key (kbd "C-c b")'show-file-name) ; or any other key you want
+
+;; Indent 
+(global-set-key (kbd "C-c i")'indent-region) ; indent
 
 ;; Comment
 (defalias 'ct 'comment-or-uncomment-region)
@@ -155,7 +170,7 @@
 (require 'ido)
 (ido-mode t)
 
-;;;; ediff
+;; ediff
 (setq ediff-split-window-function 'split-window-horizontally)
 
 ;; default emacs configuration directory
@@ -164,15 +179,32 @@
 ;; utility finction to auto-load my package configurations
 (defun yan:load-config-file (filelist)
   (dolist (file filelist)
-    (load (expand-file-name 
-	   (concat yan:emacs-config-dir file)))
-    (message "Loaded config file:%s" file)
-    ))
+	  (load (expand-file-name 
+		 (concat yan:emacs-config-dir file)))
+	  (message "Loaded config file:%s" file)
+	  ))
 
 ;; load my configuration files
 (yan:load-config-file '("mswindows_config"
 			"programming"
 			"mit_6_945"
 			))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (matlab-mode scheme-complete scala-mode2 scala-mode powershell magit git geiser fuzzy auto-complete auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 
 
